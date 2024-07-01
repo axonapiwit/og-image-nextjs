7b1913a902c2563fa0f93eeac7c42a28c66c423d
serizawa <apiwit.t@maxion.tech>
2024-07-01 16:20:00 +0700

    improve code and deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,12 +19,15 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <title>Hello world</title>
-        <meta
+        {/* <meta
           property="og:image"
-          content="http://192.168.1.104:3000/api/og"
+          content="http://localhost:3000/api/og"
+        /> */}
+          <meta
+          property="og:image"
+          content="https://img.freepik.com/free-photo/anime-style-character-space_23-2151134277.jpg?t=st=1719825547~exp=1719829147~hmac=6fa9ce7031501066d7e8ba8e2582b20644dec5fff6db8132664f45d61fd914b4&w=1800"
         />
         <meta property="og:title" content="Product Name" />
-        {/* <meta property="og:image" content="https://example.com/path/to/product-image.jpg" /> */}
         <meta property="og:description" content="Buy this amazing product for only $99.99!" />
         <meta property="og:url" content="https://example.com/product-page-url" />
       </head>
